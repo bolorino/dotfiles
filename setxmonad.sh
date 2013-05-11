2400332800f8fb2a79f3e0c8663756878ec14753
Jose Bolorino <jose.bolorino@gmail.com>
2013-05-11 16:11:41 +0200

    Xdefaults and xmonad start script updated

--- setxmonad.sh
+++ setxmonad.sh
@@ -3,16 +3,14 @@
 xcompmgr -I1 -O1 -Ff &
 
 # Set background image
-feh --bg-tile Imágenes/wallpapers/black-wood-1920x1200.jpg 
+feh --bg-tile Imágenes/wallpapers/xmonad-wp.png
 
 # Tray bar
 trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype pixel --width 190 --transparent true --tint 0x000000 --height 12 --margin 1 &
-sleep 2
+sleep 1
 
 # Background apps
 dropbox start &
-# Backups
-/usr/local/bin/junglediskdesktop &
 
 # Run terminal
 x-terminal-emulator &
