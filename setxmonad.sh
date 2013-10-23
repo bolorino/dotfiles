7a4ac79a515d74b1e7ff23f876bb83d127d9f3bb
Jose Bolorino <jose.bolorino@gmail.com>
2013-10-23 16:09:21 +0200

    Xdefaults, tmux with powerline and compton

--- setxmonad.sh
+++ setxmonad.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 # Enable composition
-xcompmgr -I1 -O1 -Ff &
+# xcompmgr -I1 -O1 -Ff &
+compton --config ~/.compton.conf &
 
 # Set background image
 feh --bg-tile Imágenes/wallpapers/xmonad-wp.png
 
 # Tray bar
-trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype pixel --width 190 --transparent true --tint 0x000000 --height 12 --margin 1 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype pixel --width 191 --transparent true --alpha 0 --tint 0x27241E --height 19 --margin 1441 &
 sleep 1
 
 # Background apps
 dropbox start &
 
 # Run terminal
-x-terminal-emulator &
+urxvt &
 
 export _JAVA_AWT_WM_NONREPARENTING=1 
