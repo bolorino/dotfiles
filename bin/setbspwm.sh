9e4d91f190ec91905e8e0854526587f04613094d
Jose Bolorino <jose.bolorino@gmail.com>
2013-11-30 19:45:01 +0100

    setbspwm trayer and volumeicon

--- bin/setbspwm.sh
+++ bin/setbspwm.sh
@@ -9,13 +9,14 @@ wmname LG3D
 feh --bg-tile $HOME/Imágenes/wallpapers/capheine.png
 
 # Tray bar
-trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype pixel --width 191 --transparent true --alpha 0 --tint 0x27241E --height 19 --margin 1441 &
+trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype pixel --width 191 --transparent true --alpha 0 --tint 0x232C31 --height 18 --margin 1441 &
 
 # pypanel &
 
 #Python version fix for some programs
 PYTHONPATH=/usr/lib/python2.7/site-packages
 dropboxd &
+volumeicon &
 ubuntuone-launch &
 
 export PYTHONPATH=/usr/lib/python3.3/site-packages
