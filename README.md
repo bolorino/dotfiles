87edac8390803358d220fb088eceab859aeafd47
Jose Bolorino <jose.bolorino@gmail.com>
2015-11-02 23:11:34 +0100

    Some cleanup

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 Some Linux config files
 =======================
 
-Personal configuration files.
+Personal configuration files and scripts.
 
 * [bspwm] (https://github.com/baskerville/bspwm)
 * [bar] (https://github.com/LemonBoy/bar)
-* [Xmonad] (http://www.haskell.org/haskellwiki/Xmonad)
 * [Tmux] (http://tmux.sourceforge.net/)
-* [Xmobar] (http://projects.haskell.org/xmobar/)
 * [Vim] (http://www.vim.org/)
--- bin/setbspwm.sh
+++ bin/setbspwm.sh
@@ -1,26 +1,36 @@
 #! /bin/sh
+# xrandr layout
+#.screenlayout/leftof.sh &
+
 # Enable composition
 compton --config ~/.compton.conf &
 
 # Java windows
 wmname LG3D
 
+# Terminal
+TERMINAL=xfce4-terminal
+
 # Set background image
 wallpaper.sh
 
+# Restrict Huion graphic table to main monitor
+xinput set-prop "1060PRO Pen" --type=float "Coordinate Transformation Matrix" 0.571428 0 0.428571 0 1 0 0 0 1
+
 # Tray bar
-trayer --edge top --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype pixel --width 135 --transparent true --alpha 0 --tint 0x232C31 --height 18 --margin 1441 &
+trayer --edge bottom --align right --SetDockType true --SetPartialStrut true  --expand true --widthtype requested --transparent true --alpha 0 --tint 0x333333 --height 18 --margin 10 &
 
 #Python version fix for some programs
 PYTHONPATH=/usr/lib/python2.7/site-packages
-dropboxd &
+
+#Enable video acceleration
+export VDPAU_DRIVER=radeonsi
+
+owncloud &
 volumeicon &
-album-art.py &
 
-conky -c ~/.conky/withcovers/.conkyrc &
-
-export PYTHONPATH=/usr/lib/python3.3/site-packages
+#PYTHONPATH=/usr/lib/python3.4/site-packages
 
 # Apps
-urxvt -e zsh -c "maintmux" &
-firefox &
+rofi -key-run mod4+F2 -key-ssh mod4+F7 -key-window mod4+F5 -terminal urxvt &
+$TERMINAL --command "maintmux" &
