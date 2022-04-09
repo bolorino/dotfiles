39f09e59aa548dfd3ef82214a4b9582415135edf
Jose Bolorino <jose.bolorino@gmail.com>
2022-04-09 21:07:17 +0200

    Bspwm & Polybar with two monitors

--- trantor/polybar/launch.sh
+++ trantor/polybar/launch.sh
@@ -6,5 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-polybar -r top &
+# Launch one bar per monitor
+polybar -c ~/.config/polybar/config -r top &
+polybar -c ~/.config/polybar/config -r top_extra &
