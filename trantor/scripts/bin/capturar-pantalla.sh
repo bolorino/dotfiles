5a617889bb730116de7e2ef42ad62d85efc28876
Jose Bolorino <jose.bolorino@gmail.com>
2024-02-14 23:15:57 +0100

    Remove old files

--- trantor/scripts/bin/capturar-pantalla.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scrot $HOME/'Imágenes/capturas/%Y-%m-%d_%H%M%S-$wx$h_scrot.png'
-notify-send 'scrot' 'Captura tomada'
--- trantor/scripts/bin/wallpaper.sh
+++
@@ -1,4 +0,0 @@
-#wallpaper.sh
-WALLPAPERS="${HOME}/sync/wallpapers/"
-
-feh --bg-scale "${WALLPAPERS}$(ls ${WALLPAPERS} | sort -R | head -1)"
